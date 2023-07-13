2ca6b39ae785e4058714cd3add23040b105ff474
Donier Rakhimov <Rdonik0807@yandex.ru>
2023-07-13 22:10:51 +0300

    added cors

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { errors, celebrate } = require('celebrate');
 const bodyParser = require('body-parser');
 const { errorLogger } = require('express-winston');
+const cors = require('cors');
 
 const { requestLogger } = require('../middlewares/logger');
 const { registerValidator, loginValidator } = require('../utils/validators');
@@ -11,10 +12,11 @@ const { userRouter } = require('./users');
 const { movieRouter } = require('./movies');
 const NotFoundError = require('../utils/errorClasses/NotFoundError');
 const globalHandler = require('../middlewares/globalHandler');
+const corsOptions = require('../utils/corsOptions');
 
 router.use(requestLogger);
-// router.use(cors(corsOptions));
-// router.options('*', cors(corsOptions));
+router.use(cors(corsOptions));
+router.options('*', cors(corsOptions));
 router.use(bodyParser.json());
 router.post('/signup', celebrate(registerValidator), register);
 router.post('/signin', celebrate(loginValidator), login);
